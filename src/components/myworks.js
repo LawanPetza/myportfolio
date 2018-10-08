39960c6cd7fad3677d35cb543f5e5e47e363fe6b
Lawan Petza <Lawan.Petza@gmail.com>
2018-10-08 02:20:39 -0400

    First commit

---
+++ src/components/myworks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Mywork extends Component {
+    render() {
+        return(
+            <div><h1>Mywork Page</h1></div>
+
+        )
+    }
+}
+
+export default Mywork;

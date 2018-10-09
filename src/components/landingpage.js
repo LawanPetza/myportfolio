9482c7035c3a53735436eefb282da812d7531096
Lawan Petza <Lawan.Petza@gmail.com>
2018-10-08 20:59:50 -0400

    modified components

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -8,24 +8,34 @@ class Landing extends Component {
             <div style={{ width: '100%', margin: 'auto' }}>
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                        <img 
-                        src="https://previews.123rf.com/images/magurok/magurok1706/magurok170600030/79971164-businesswoman-office-manager-banker-consultant-avatar-profile-concepts-cute-female-character-with-lo.jpg"
-                        alt="character"
-                        className="character-img"
+                        <img
+                            src={require("../image/myPic.JPG")}
+                            alt="character"
+                            className="character-img"
                         />
 
                         <div className="banner-text">
-                        <h1>Full Stack Web Developer</h1>
-                        <hr/>
-                        <p>HTML5/CSS3 | JavaScript | jQuery | Bootstrap | React.js | Node.js | Express | MongoDB | MySQL | </p>
+                            <h1>Full Stack Web Developer</h1>
+                            <hr />
+                            <p>HTML5/CSS3 | JavaScript | jQuery | Bootstrap | React.js | Node.js | Express | MongoDB | MySQL | </p>
+
+                            <div className="social-links">
+
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/lawanpetza" rel="noopener noreferer" target="blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                                </a>
+
+                                {/* Github */}
+                                <a href="https://github.com/LawanPetza" rel="noopener noreferer" target="blank">
+                                    <i className="fa fa-github" aria-hidden="true"></i>
+                                </a>
+
+                            </div>
 
                         </div>
-
                     </Cell>
                 </Grid>
-
-
-
             </div>
 
         )
